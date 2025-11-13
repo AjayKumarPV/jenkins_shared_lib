5daf139d44588e098d5a8c8dd0f85441937cbed5
AjayKumarPV <ajkonemail@gmail.com>
2025-11-13 17:18:23 +0530

    Update SonarQubeImageAnalysis.groovy

--- vars/SonarQubeImageAnalysis.groovy
+++ vars/SonarQubeImageAnalysis.groovy
@@ -2,8 +2,7 @@ def call(credentialsId){
 
     withSonarQubeEnv(credentialsId: credentialsId) {
          sh """
-            mvn clean package sonar:sonar \
-              -Dsonar.dependencyCheck.reportPath=trivy-results.sarif
+            mvn clean package sonar:sonar
          """
     }
 }
