e5638d126ec5ed421fb1c507a8bc4de017a0d9b4
AjayKumarPV <ajkonemail@gmail.com>
2025-11-13 16:05:41 +0530

    Update dockerImageScan.groovy

--- vars/dockerImageScan.groovy
+++ vars/dockerImageScan.groovy
@@ -1,9 +1,10 @@
 def call(String project, String ImageTag, String hubUser){
     
     sh """   
-     trivy image ${hubUser}/${project}:${ImageTag} > scan.txt
-     cat scan.txt
+      trivy image --exit-code 1 --severity HIGH,CRITICAL ${hubUser}/${project}:${ImageTag}
+      trivy image --format table --output scan.txt ${hubUser}/${project}:${ImageTag}
     """
+    archiveArtifacts artifacts: 'scan.txt', fingerprint: true
 }
 
 // def call(String aws_account_id, String region, String ecr_repoName){
@@ -12,4 +13,5 @@ def call(String project, String ImageTag, String hubUser){
 //     trivy image ${aws_account_id}.dkr.ecr.${region}.amazonaws.com/${ecr_repoName}:latest > scan.txt
 //     cat scan.txt
 //     """
-// }
+// } trivy image ${hubUser}/${project}:${ImageTag} > scan.txt
+// cat scan.txt
